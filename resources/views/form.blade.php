b1c8c041d6f9aac80ef4593be97c7faa55c8f8d0
ajwoody007 <andy@andywood.net>
2024-04-25 14:04:07 +0100

    laravel update 1

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -24,7 +24,6 @@
             @method('PUT')
         @endisset
 
-<<<<<<< HEAD
         <div>
             <label for="name">
                 Name
@@ -53,73 +52,14 @@
 
         <div>
             <button type="submit">
-=======
-
-        <table>
-
-            <tr>
-
-                <td><label for="name">Name: </label></td>
-                <td> 
-                    <input text="text" name="name" id="name" value="{{ $contact->name ?? old('name') }}"/>
-                    @error('name')
-                        <p class='error_message'>{{ $message }}</p>
-                    @enderror
-                </td>
-
-            </tr>
-
-            <tr>
-
-                <td><label>Email :</label></td>
-
-                <td>
-                    <input text="text" name="email" id="email" value="{{ $contact->email ?? old('email') }}"/>
-                    @error('email')
-                        <p class='error_message'>{{ $message }}</p>
-                    @enderror
-                </td>
-
-            </tr>
-
-            <tr>
-
-                <td><label>Mobile :</label></td>
-                <td>
-
-                    <input text="text" name="mobile" id="mobile" value="{{ $contact->mobile ?? old('mobile') }}"/>
-                    @error('mobile')
-                        <p class='error_message'>{{ $message }}</p>
-                    @enderror
-
-                </td>
-
-            </tr>
-
-        <table>
-
-        <div>
-            <button class='app_button mt-4' type="submit">
->>>>>>> 6f08307 (laravel update 1)
                 @isset($contact)
                     Update Contact
                 @else
                     Add Contact
                 @endisset
             </button>
-<<<<<<< HEAD
-=======
-            <br><br>
-            <a class='link mt-4' href="{{ route('contacts.index') }}">Cancel</a>
->>>>>>> 6f08307 (laravel update 1)
         </div>
     
     </form>
 
 @endsection
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 6f08307 (laravel update 1)
